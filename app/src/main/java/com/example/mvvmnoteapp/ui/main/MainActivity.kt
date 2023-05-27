f7d6a95ed08704e9d164741fb080e45904507081
TheRipperX <ripper.cs-ret@hotmail.com>
2023-05-27 12:31:00 +0330

    add folders and fix gradle

--- app/src/main/java/com/example/mvvmnoteapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/mvvmnoteapp/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.mvvmnoteapp
+package com.example.mvvmnoteapp.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mvvmnoteapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
