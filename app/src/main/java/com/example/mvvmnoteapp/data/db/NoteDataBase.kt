6e7d7b74faccf76ed2ba32b098364fd30eb7a2fc
TheRipperX <ripper.cs-ret@hotmail.com>
2023-05-27 13:49:02 +0330

    fix app error gradle

--- app/src/main/java/com/example/mvvmnoteapp/data/db/NoteDataBase.kt
+++ app/src/main/java/com/example/mvvmnoteapp/data/db/NoteDataBase.kt
@@ -6,7 +6,7 @@ import com.example.mvvmnoteapp.data.model.NoteEntity
 import com.example.mvvmnoteapp.utils.DataBase_Version
 
 
-@Database(entities = [NoteEntity::class], version = DataBase_Version)
+@Database(entities = [NoteEntity::class], version = DataBase_Version, exportSchema = false)
 abstract class NoteDataBase: RoomDatabase() {
     abstract fun noteDao(): NoteDao
 }
